994d0b392f6f53c77f6021d62b8875d4e539915c
Madeleine Yael <myael@student.21-school.ru>
2022-07-27 11:02:39 +0500

    fix — add create table if not exist, other small fixes

--- ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
+++ ex01/Tests/src/test/java/edu/school21/repositories/EmbeddedDataSourceTest.java
@@ -1,21 +1,22 @@
 package edu.school21.repositories;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 public class EmbeddedDataSourceTest {
-    private DataSource dataSource;
+    private EmbeddedDatabase dataBase;
 
     @BeforeEach
     void init()
     {
-        dataSource = new EmbeddedDatabaseBuilder()
+        dataBase = new EmbeddedDatabaseBuilder()
                 .setName("Test")
                 .setType(HSQL)
                 .addScript("/schema.sql")
@@ -25,7 +26,13 @@ public class EmbeddedDataSourceTest {
 
     @Test
     void testConnection() throws SQLException {
-        Assertions.assertNotNull(dataSource.getConnection());
+        Assertions.assertNotNull(dataBase.getConnection());
+    }
+
+    @AfterEach
+    void close() throws SQLException {
+        dataBase.getConnection().close();
+        dataBase.shutdown();
     }
 
 }
